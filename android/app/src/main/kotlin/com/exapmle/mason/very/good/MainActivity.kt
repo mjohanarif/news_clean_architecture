e241badbbe0fdde406dd8d8534928314601cc684
mjohanarif <m.johanarifin@gmail.com>
2023-10-28 08:59:06 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/exapmle/mason/very/good/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.exapmle.mason.very.good.mason_very_good
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,10 @@
+import 'package:mason_very_good/bootstrap.dart';
+import 'package:mason_very_good/common/variable.dart';
+import 'package:mason_very_good/presentation/presentation.dart';
+
+void main() {
+  bootstrap(
+    () => const NewsPage(),
+    Flavor.production,
+  );
+}
